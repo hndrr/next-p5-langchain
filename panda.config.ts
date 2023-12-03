ac1bbb2a1ceb60913aba6dced6edc4c161f4969a
hndrr <yozhik20@gmail.com>
2023-12-03 15:50:44 +0900

    add ark/park ui

--- panda.config.ts
+++ panda.config.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   // Whether to use css reset
   preflight: true,
 
+  // Presets to use
+  presets: ["@pandacss/preset-base", "@park-ui/panda-preset"],
+
   // Where to look for your css declarations
   include: [
     "./src/components/**/*.{ts,tsx,js,jsx}",
@@ -17,6 +20,9 @@ export default defineConfig({
   theme: {
     extend: {},
   },
+
+  // The framework you are using
+  jsxFramework: "react",
 
   // The output directory for your css system
   outdir: "styled-system",
